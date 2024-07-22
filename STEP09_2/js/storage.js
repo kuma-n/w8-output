0e2ae742c7c1ca539ca7caff424519c9a6dbeac5
kuma-n <r201304504uo@gmail.com>
2024-07-23 00:34:44 +0900

    app.jsからstorage.jsへファイル名変更

---
+++ STEP09_2/js/storage.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById('js-login-form');
+const emailInput = document.getElementById('js-login-email');
+const passwordInput = document.getElementById('js-login-password');
+const signupLink = document.getElementById('js-signup-link');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    localStorage.setItem('saveEmail', emailInput.value);
+    localStorage.setItem('savePassword', passwordInput.value);
+}, {passive: false});
+
+signupLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    emailInput.value = localStorage.getItem('saveEmail');
+    passwordInput.value = localStorage.getItem('savePassword');
+}, {passive: false});
